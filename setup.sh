08ff8e8e38f277d8e399236afadf6b37e19f7413
teekan <82342440+teekan@users.noreply.github.com>
2021-04-11 17:13:19 -0700

    Update and rename setup to setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+apt -y update
+apt install -y mysql-server apache2 php libapache2-mod-php php-mysql tar ffmpeg
+tar -xf archive.tar
+rm archive.tar
+mv ./site/* /var/www/html/
+rm -r site
+echo "create database avs" | mysql -u root -p
+mysql -u root -p avs < /var/www/html/avs.sql
+rm /var/www/html/avs.sql
+echo "extension=php_mysqli.dll" >> /etc/php/7.0/apache2/php.ini
+chmod 777 /var/www/html/include/config.local.php
+chmod -R 777 /var/www/html/cache/frontend
+chmod -R 777 /var/www/html/cache/backend
+chmod -R 777 /var/wwww/html//images/
+chmod -R 777 /var/www/html/media/
+chmod -R 777 /var/www/html/templates/
+chmod -R 777 /var/www/html/tmp/
+chmod 777 /var/www/html/aembed.sh
+service apache2 restart
